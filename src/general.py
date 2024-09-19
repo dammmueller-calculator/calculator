c34f7012dea82776550eb9fcdf5aa62893ee0d31
Florian Mros <mail@floork.dev>
2024-09-19 12:03:19 +0200

    add: general

---
+++ src/general.py
@@ -0,0 +1,5 @@
+'''
+this is just a placeholder file at the moment,
+but later on her in this directory will the code which has nothing to do with the look/view of the project
+will be stored in here (better in multiple files in this dir)
+'''
