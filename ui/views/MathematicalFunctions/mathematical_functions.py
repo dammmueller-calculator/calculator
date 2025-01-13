5b49a81d737f3db6418964d4c61f31306fc99e64
Träber <i22traeberce>
2025-01-13 12:16:19 +0100

    mathematical-functions - add classes and ui file

---
+++ ui/views/MathematicalFunctions/mathematical_functions.py
@@ -0,0 +1,22 @@
+from PyQt6 import uic
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QApplication
+
+from ui.views.InputModule import InputModule
+
+
+class MathematicalFunctions(QWidget):
+    def __init__(self, parent=None):
+        super().__init__()
+        self.parent = parent
+
+        uic.loadUi("ui/views/percent/percent.ui", self)
+
+        input_widget = self.findChild(QWidget, "wi_inputModule")
+        layout = QVBoxLayout()
+        input_widget.setLayout(layout)
+
+        self.input_module = InputModule()
+        layout.addWidget(self.input_module)
+
+    def handle_input_on_function_select(self):
+        return
