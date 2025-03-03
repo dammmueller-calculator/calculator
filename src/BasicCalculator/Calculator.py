8e591c6ef93f7ff9814e1a7e37fc657a8f7e3782
stefan klaeck <sklaeck10227@cosmoconsult.com>
2025-03-03 07:38:04 +0100

    Update

--- src/BasicCalculator/Calculator.py
+++ src/BasicCalculator/Calculator.py
@@ -1,68 +1,21 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLineEdit
-from PyQt6.QtCore import pyqtSignal
-from ui.views.InputModule import InputModule
+def add(x, y):
+    return x + y
 
-class CalculatorModule(QWidget):
-    def __init__(self):
-        super().__init__()
-        
-        # Layout für den Taschenrechner
-        layout = QVBoxLayout(self)
-        
-        # Eingabefeld
-        self.input_line = QLineEdit(self)
-        self.input_line.setReadOnly(True)  # Das Eingabefeld ist schreibgeschützt
-        self.input_line.setText("0")  # Standardwert
-        
-        # InputModule hinzufügen
-        self.input_module = InputModule()
-        
-        # Layout zusammenfügen
-        layout.addWidget(self.input_line)
-        layout.addWidget(self.input_module)
-        self.setLayout(layout)
+def checksign(sign):
+    # Klammer
+    # Case 1: sign = "("    
+    if sign == "*":
+        return "*"
+    elif sign == "÷":
+        return "/"
+    elif sign == "−":
+        return "-"
+    elif sign == "+":
+        return "+"
+    elif sign == "=":
+        return "="
+    else:
+        return sign
 
-        # Signale verbinden
-        self.input_module.number_pressed.connect(self.append_to_expression)
-        self.input_module.sign_pressed.connect(self.process_sign)
 
-        # Interne Variable für den aktuellen Ausdruck
-        self.current_expression = ""
-
-    def append_to_expression(self, value):
-        """Fügt Zahlen oder Punkt/Komma zur Eingabe hinzu."""
-        if self.current_expression == "0":  # Anfangszeichen "0" ersetzen
-            self.current_expression = ""
-        self.current_expression += value
-        self.update_display()
-
-    def process_sign(self, sign):
-        """Bearbeitet Rechenoperationen oder spezielle Zeichen."""
-        if sign == "=":
-            self.calculate_result()
-        elif sign == "C":
-            self.clear_expression()
-        else:
-            self.current_expression += f" {sign} "
-            self.update_display()
-
-    def calculate_result(self):
-        """Berechnet das Ergebnis des aktuellen Ausdrucks."""
-        try:
-            # Ersetze Komma durch Punkt für Dezimalzahlen
-            expression = self.current_expression.replace(",", ".")
-            result = eval(expression)  # Vorsicht bei eval, besser nur für kontrollierte Ausdrücke
-            self.current_expression = str(result).replace(".", ",")  # Rückumwandlung für Anzeige
-            self.update_display()
-        except Exception:
-            self.current_expression = "Error"
-            self.update_display()
-
-    def clear_expression(self):
-        """Setzt den aktuellen Ausdruck zurück."""
-        self.current_expression = "0"
-        self.update_display()
-
-    def update_display(self):
-        """Aktualisiert das Eingabefeld."""
-        self.input_line.setText(self.current_expression)
+       
--- ui/views/BasicModule/BasicModule.py
+++ ui/views/BasicModule/BasicModule.py
@@ -1,8 +1,13 @@
 from PyQt6 import uic
 from PyQt6.QtWidgets import QWidget, QVBoxLayout
 from ui.views.InputModule import InputModule
+from src.BasicCalculator.Calculator import add
 
 class BasicModule(QWidget):
+
+    # Global Variables
+    value_list = []
+
     def __init__(self):
         super().__init__()
         uic.loadUi("ui/views/BasicModule/BasicModule.ui", self)      
@@ -16,9 +21,40 @@ class BasicModule(QWidget):
         layout.addWidget(self.input_module)
 
         self.input_module.number_pressed.connect(self.update_inputLine)
-        self.input_module.sign_pressed.connect(self.update_inputLine)
+        self.input_module.sign_pressed.connect(self.check_sign)
 
     def update_inputLine(self, value):
+        #self.value_list.append(value)       
         text = self.inputLine.text()
         self.inputLine.setText(text + value)
+
+    def check_sign(self,sign):
+    # Klammer
+    # Case 1: sign = "("    
+        if sign == "*":
+            return "*"
+        elif sign == "÷":
+            self.value_list.append(self.inputLine.text())
+            self.value_list.append("+")
+            self.inputLine.setText('')
+            return "/"
+        elif sign == "−":
+            return "-"
+        elif sign == "+":
+            self.value_list.append("+")
+            return "+"
+        elif sign == "=":
+        
+            return "="
+            # hier könnte jetzt eine Funktion stehen
+        else:
+            return sign
+
+
+    # schreibe eine Funktion die die Liste auseinander nimmt und die Rechnung durchführt
+
+    # wenn eine Opertor gedrückt wird, dann nimm Text trage in Liste Lösche Inputline
+
+    
+       
 
